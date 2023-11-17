189840e24d93ae83d43e8b3b69effc65604dba4e
Ebenclaw <rebekasanper15@gmail.com>
2023-11-17 12:19:46 +0100

    Version1.0

---
+++ README.md
@@ -0,0 +1,14 @@
+# Proyecto DWEC
+## Autor
+Rebeca Sánchez Pérez
+## Requisitos minimos del desarrollo
+- [Apache2.4.52]
+- [MySQL8.0.35]
+- [PHP8.1]
+- [XDebug3.1.2]
+## Descripcion
+Documentacion de la asignatura de desarrollo web entorno cliente
+## Funcionalidades
+## Entorno de desarrollo
+[http://daw204.isauces.local/204DWECProyectoDWEC/indexProyectoDWEC.php](http://daw204.isauces.local/204DWECProyectoDWEC/indexProyectoDWEC.php)
+## Entorno de explotacion
---
+++ indexProyectoDWEC.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="webroot/css/style.css">
+    <link rel="icon" type="image/x-icon" href="/webroot/image/flora.png">
+    <title>Rebeca Sánchez Pérez</title>
+</head>
+
+<body>
+    <header>
+        <a href="../index.html"><img id="logo" src="webroot/image/logo.png" alt="Logo"></a>
+        <h1>Desarrollo Web en entorno Cliente - DWEC</h1>
+    </header>
+    <main>
+        <table>
+            <caption>Prácticas</caption>
+            <tr>
+                <td>Práctica 2.1 - Trabajando con bucles</td>
+                <td><a href="practicas/2-1.html" target="_blank"><img src="webroot/image/play.png" alt="play"/></a></td>
+                <td><a href="doc/Práctica2.1.pdf" target="_blank"><img src="webroot/image/ojo.png" alt="ojo"/></a></td>
+            </tr>
+            <tr>
+                <td>Práctica 3.1 - Trabajando con arrays, conjuntos y mapas</td>
+                <td></td>
+                <td><a href="doc/Práctica3.1.pdf" target="_blank"><img src="webroot/image/ojo.png" alt="ojo"/></a></td>
+            </tr>
+            <tr>
+                <td>..</td>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+                <td>..</td>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+                <td>..</td>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+                <td>..</td>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+                <td>..</td>
+                <td></td>
+                <td></td>
+            </tr>
+        </table>
+    </main>
+    <footer>
+        <div id="derechos">2023-2024 © Todos los derechos reservados: <a href="../index.html">Rebeca Sánchez Pérez</a></div>
+        <div id="fotos">
+            <a href="https://github.com/Ebenclaw" target="_blank"><img id="git" src="webroot/image/GitHub.png" alt="GitHub"></a>
+            <a href="http://ieslossauces.centros.educa.jcyl.es/sitio/" target="_blank"><img id="sauces" src="webroot/image/sauces.png" alt="Sauces"></a>
+            <a href="../index.html"><img id="home" src="webroot/image/home.png" alt="Inicio"></a>
+        </div>
+    </footer>
+</body>
+
+</html>
+
